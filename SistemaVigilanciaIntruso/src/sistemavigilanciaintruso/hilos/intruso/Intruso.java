c5fff3fdaa659434fec289dc8e0aeb53ff60eb0c
Dr4g0nJ4r <luis.keller@est.fi.uncoma.edu.ar>
2020-09-12 20:26:21 -0300

    Update Intruso
    Se agregaron las variables de instancia:
    - EstadoIntruso estado;
        Museo museo;
        private int nroSalaActual;
        private StringBuilder informe;
        private int[] salasRecorridas;
    
    - Se agregaron los métodos

--- SistemaVigilanciaIntruso/src/sistemavigilanciaintruso/hilos/intruso/Intruso.java
+++ SistemaVigilanciaIntruso/src/sistemavigilanciaintruso/hilos/intruso/Intruso.java
@@ -17,18 +17,91 @@ import sistemavigilanciaintruso.hilos.intruso.estados.EstadoIntruso;
 public class Intruso extends Persona implements EstadoIntruso{
     EstadoIntruso estado;
     Museo museo;
+    private int nroSalaActual;
+    private StringBuilder informe;
+    private int[] salasRecorridas;
+    
+    
     public Intruso(Museo m){
         this.estado = new Recorrer();
         this.museo = m;
+        int nroSalaActual = (int) (Math.random() * museo.getCantidad() - 1) + 1;  //Rango del 1 a la cantidad de salas menos uno (index 0 es la sala de vigilancia)
     }
     
     @Override
-    public String call(){
-        String informe = "";                                                    //variable que sirve para armar un informe sobre las actividades del intruso.
+    public StringBuilder call(){                                                  //variable que sirve para armar un informe sobre las actividades del intruso.
         boolean termina = false;   
-        System.out.println("soy intruso");                                      //variable que me pemite controlar si el intruso termina sus actividades.
-        
-        informe = "Informe del Intruso";
+        informe.append("Informe del Intruso");                                     //variable que me pemite controlar si el intruso termina sus actividades.
+        while(!termina)
+        {
+            System.out.println("Intruso: Estoy en ");
+        }
         return informe;
+    }
+
+    @Override
+    public boolean accion(Intruso persona) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getTiempo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getNombreEstado() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+ 
+    
+    public boolean entrarEnSalaMuseo(int nro){
+        return this.museo.entrarASala(nro);
+    }
+    
+    public boolean salirDeSalaMuseo(int nro){
+        return this.museo.salirSala(nro);
+    }
+    
+    public void actualizarInforme(String dato){
+        this.informe.append(dato+"\n");
+    }
+    
+    public Museo getMuseo(){
+        
+        return this.museo;
+    }
+    
+    public void setEstado(EstadoIntruso e){
+        this.estado=e;
+    }
+    
+    public String obtenerNombreSalaMuseo(int nro){
+        return this.museo.obtenerNombreSala(nro);
+    }
+    
+    public boolean alguienSalaMuseo(int nro){
+        return this.museo.hayAlguienEnSala(nro);
+    }
+    
+    public int obtenerValorSalaMuseo(int nro){
+        return this.museo.obtenerValorSala(nro);
+    }
+    
+     public int getCantSalasRecorridas(){
+        return 1;
+    }
+    
+    public void setCantidadSalasRecorridas(int cant){
+        
+    }
+    public void setNroSalaActual(int n){
+        this.nroSalaActual=n;
+    }
+    public int getNroSalaActual(){
+        return nroSalaActual;
+    }
+    public boolean esCerradoMuseo(){
+        return this.museo.EsCerrado();
     }
 }
