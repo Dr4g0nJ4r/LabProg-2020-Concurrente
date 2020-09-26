807a43236297bb8da944d3907636b5517a356a3b
UlisesJaramillo <70350519+UlisesHumbertoJaramillo@users.noreply.github.com>
2020-09-25 22:53:11 -0300

    no me acepta poner el cierre del servicio en el finally

--- SistemaVigilanciaIntruso/src/sistemavigilanciaintruso/SistemaVigilanciaIntruso.java
+++ SistemaVigilanciaIntruso/src/sistemavigilanciaintruso/SistemaVigilanciaIntruso.java
@@ -35,13 +35,13 @@ public class SistemaVigilanciaIntruso {
             System.out.println("Informes:");
             System.out.println(informeIntruso.get());
             System.out.println(informeVigilante.get());
-            
+            servicio.shutdown();                                                //cuando los hilos se detengan cierro el servicio.
            
         } catch (Exception e) {
             //TODO: handle exception
             
         }finally{
-            servicio.shutdown();                                                //cuando los hilos se detengan cierro el servicio.
+            
 
         }
     }
